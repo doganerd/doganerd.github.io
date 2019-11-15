d66766f2b72667536016d44b9f0f85e5e4e5ff6f
doganerd <dilara.doganer@hs-furtwangen.de>
2019-11-15 17:18:14 +0100

    aufg5

---
+++ Aufgabe5/skript.js
@@ -0,0 +1,8 @@
+console.log("test");
+var zahl1 = 200;
+var zahl2 = 20;
+var ergebnis = (zahl2 / zahl1) * 100;
+var prozent = "%";
+console.log(ergebnis + prozent);
+console.log(ergebnis + "%");
+//# sourceMappingURL=skript.js.map
---
+++ Aufgabe5/skript.ts
@@ -0,0 +1,11 @@
+console.log("test");
+
+var zahl1:number = 200;
+var zahl2:number = 20;
+var ergebnis:number = (zahl2/zahl1)*100;
+var prozent:string = "%";
+
+console.log(ergebnis+prozent);
+
+
+console.log(ergebnis+"%");
